9caa69d50d6d80cf7dd8914174f5c62ee19450ec
Melos89 <Nicolausfugler_89@hotmail.com>
2015-12-29 15:54:48 +0100

    function 3 commit 2

--- Labb1/Labb1/Scripts/Indexpunkt3.js
+++ Labb1/Labb1/Scripts/Indexpunkt3.js
@@ -4,3 +4,13 @@ var secondNumber = prompt("Second number");
 
 var thirdNumber = prompt("third number");
 
+sumUp(firstnumber, secondNumber, thirdNumber);
+
+
+
+
+
+function sumUp(a, b, c)
+{
+    document.getElementById('result').innerHTML = "Totala summan: " + (a + b + c);
+}
