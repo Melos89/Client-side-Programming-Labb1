3f27f44fbdd72e3539d51844a45b3789c9c4664c
Melos89 <Nicolausfugler_89@hotmail.com>
2015-12-29 14:29:44 +0100

    function 2 commit 1

---
+++ Labb1/Labb1/Scripts/Indexpunkt2.js
@@ -0,0 +1,16 @@
+﻿var number = prompt("supply a number:");
+
+
+if (EvenDivision(number))
+
+
+
+
+
+function EvenDivision(nr)
+{
+    if(nr % 3 == 0)
+        return true;
+    else
+        return false;
+}
