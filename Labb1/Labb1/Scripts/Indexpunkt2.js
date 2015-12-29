e9c99a472a546b15d35f31fa57829bcc3c3c8c71
Melos89 <Nicolausfugler_89@hotmail.com>
2015-12-29 14:38:56 +0100

    function 2 commit 2

--- Labb1/Labb1/Scripts/Indexpunkt2.js
+++ Labb1/Labb1/Scripts/Indexpunkt2.js
@@ -1,7 +1,12 @@
 ﻿var number = prompt("supply a number:");
 
+var result = EvenDivision(number);
 
-if (EvenDivision(number))
+
+if (result == true)
+    document.getElementById('result').innerHTML = number + " är jämnt delbart med 3";
+else
+    document.getElementById('result').innerHTML = number + " är inte jämnt delbart med 3";
 
 
 
