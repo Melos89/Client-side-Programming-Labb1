29b258aac1ed8fc0192b5d916bb93deed032fc8e
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-02 09:12:47 +0100

    Function 4 commit 1

--- Labb1/Labb1/Scripts/Indexpunkt4.js
+++ Labb1/Labb1/Scripts/Indexpunkt4.js
@@ -1,1 +1,17 @@
-﻿
+﻿var number = prompt("Provide digit to determine if it´s even or not:");
+
+
+
+
+
+
+    
+
+
+function CheckIfEven(nr)
+{
+    if (nr % 2 == 0)
+        return nr + " is even";
+    else
+        return nr + " is uneven";
+}
