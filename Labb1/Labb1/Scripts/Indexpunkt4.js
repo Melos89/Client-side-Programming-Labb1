68dd51704a685d20e5bf222fcffc4d2049a29cce
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-02 09:13:33 +0100

    Function 4 commit 2

--- Labb1/Labb1/Scripts/Indexpunkt4.js
+++ Labb1/Labb1/Scripts/Indexpunkt4.js
@@ -1,11 +1,7 @@
 ﻿var number = prompt("Provide digit to determine if it´s even or not:");
 
+document.getElementById('result').innerHTML = CheckIfEven(number);
 
-
-
-
-
-    
 
 
 function CheckIfEven(nr)
