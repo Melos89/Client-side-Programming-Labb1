122c15f5bddccfa06ff2f69abb532d9a6017a24e
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-04 14:28:08 +0100

    lab 2 initial commit

---
+++ Labb1/Labb1/Scripts/Indexpunkt5.js
@@ -0,0 +1,15 @@
+﻿
+document.getElementById('result').innerHTML = CountingEvenNumbers();
+
+
+
+function CountingEvenNumbers()
+{
+    var numbers = "";
+    for (var i = 0; i <= 100; i++)
+    {
+        if(i %2 == 0)
+        numbers += i + " ";
+    }
+    return numbers;
+}
