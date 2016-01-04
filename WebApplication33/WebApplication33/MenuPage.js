b0df47b64cc2d3ff78453f4783c3c5012c06cd3d
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-04 15:14:14 +0100

    daily deal done

--- WebApplication33/WebApplication33/MenuPage.js
+++ WebApplication33/WebApplication33/MenuPage.js
@@ -22,7 +22,16 @@ for (var i = 0; i < imageholder.length; i++) {
 /*Add todays offer function. 
 Make price background red. 
 Reduce price by another 20%:*/
-
+var currentTime = new Date();
 var weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 var weekday = document.getElementsByClassName('price');
 
+for (var i = 0; i < weekday.length; i++) {
+    if(weekdays[currentTime.getDay()] === weekday[i].id)
+    {
+        var currentPrice = Number(weekday[i].innerHTML);
+        var discountedPrice = (currentPrice * 0.8).toFixed(2);
+        weekday[i].innerHTML = discountedPrice;
+        weekday[i].style.backgroundColor = "red";
+    }
+};
