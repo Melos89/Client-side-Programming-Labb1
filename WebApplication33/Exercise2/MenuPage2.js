63e1ca4ca445ab379caea8183e126467774d9230
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-07 13:42:05 +0100

    Burgers Added to page

--- WebApplication33/Exercise2/MenuPage2.js
+++ WebApplication33/Exercise2/MenuPage2.js
@@ -11,3 +11,34 @@
 */
 //DRINKS
 //Subs
+addHamburgersToPage();
+addPizzasToPage();
+addSubsToPage();
+addDrinksToPage();
+//Hamburgers
+function addHamburgersToPage() {
+    var request;
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    request.open("GET", "JSON/hamburger.json", true);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var hamburgers = JSON.parse(request.responseText);
+            var output = "<h2>" + "Delicious burgers";
+            for (var i = 0; i < hamburgers.hamburgerList.length; i++) {
+                output += "<article>" + "<fieldset>"
+                    + "<legend>" + hamburgers.hamburgerList[i].dayOfTheWeek + " burger" + "</legend>"
+                    + "<h3>" + hamburgers.hamburgerList[i].name + "</h3>"
+                    + "<span>" + "<img src='Images/Hamburger.png'/>" + "</span>"
+                    + "<div class='price'" + " id='" + hamburgers.hamburgerList[i].dayOfTheWeek + "'>" + hamburgers.hamburgerList[i].price + "</div>"
+                    + "</fieldset>" + "</article>";
+            }
+            output += "</h2>";
+            document.getElementById("burgersPlace").innerHTML = output;
+        }
+    }
+    request.send();
+}
