ba47da0546b89f94cc01235266056bc522461d39
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-07 15:14:05 +0100

    Added another attrb to JSON products, type. To use with a radio button filter, next to search function

--- Labb1/Labb3/Scripts/Index.js
+++ Labb1/Labb3/Scripts/Index.js
@@ -4,8 +4,11 @@
         var regExp = new RegExp(searchField, "i");
         $.getJSON("JSON/products.json", function (data) {
             var output = "<ul class='searchResults'>";
+            var radioFilter = $('input[name="foodType"]:checked').val();
             $.each(data, function (key, val) {
-                if (val.name.search(regExp) !== -1) {
+                if (val.name.search(regExp) !== -1 ||
+                    (val.about.search(regExp) !== -1) &&
+                    radioFilter ==) {
                     output += "<li>"
                         + "<p>" + val.name + "</p>"
                         + "<p>" + val.about + "</p>"
