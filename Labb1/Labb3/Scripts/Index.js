66d54cc46df20f08fd4d173fd9ca715fe8197315
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-07 14:49:27 +0100

    Trying to get my search filter to work

--- Labb1/Labb3/Scripts/Index.js
+++ Labb1/Labb3/Scripts/Index.js
@@ -1,1 +1,20 @@
-﻿
+﻿$(document).ready(function () {
+    $("#search").keyup(function () {
+        var searchField = $(this).val();
+        var regExp = new RegExp(searchField, "i");
+        $.getJSON("JSON/products.json", function (data) {
+            var output = "<ul class='searchResults'>";
+            $.each(data, function (key, val) {
+                if (val.name.search(regExp) !== -1) {
+                    output += "<li>"
+                        + "<p>" + val.name + "</p>"
+                        + "<p>" + val.about + "</p>"
+                        + "<img src='" + val.image + "' alt='" + val.name + "'/>"
+                        + "</li>";
+                }
+            });
+            output += "</ul>";
+            $("#update").html(output);
+        });
+    });
+})
