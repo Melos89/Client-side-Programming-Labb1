eda420fb3e2653af133dcbb21f79883d49cd2af5
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-07 15:28:04 +0100

    Getting closer in finding a good filter solution

--- Labb1/Labb3/Scripts/Index.js
+++ Labb1/Labb3/Scripts/Index.js
@@ -1,14 +1,37 @@
 ﻿$(document).ready(function () {
+
     $("#search").keyup(function () {
         var searchField = $(this).val();
         var regExp = new RegExp(searchField, "i");
         $.getJSON("JSON/products.json", function (data) {
             var output = "<ul class='searchResults'>";
-            var radioFilter = $('input[name="foodType"]:checked').val();
+
+            $.each(data, function (key, val) {
+                if (val.name.search(regExp) !== -1 ||
+                    (val.about.search(regExp) !== -1)) {
+                    output += "<li>"
+                        + "<p>" + val.name + "</p>"
+                        + "<p>" + val.about + "</p>"
+                        + "<img src='" + val.image + "' alt='" + val.name + "'/>"
+                        + "</li>";
+
+                }
+            });
+            output += "</ul>";
+            $("#Information").html(output);
+        });
+    });
+    $("#radio").change(function () {
+        var searchField = $(this).val();
+        var regExp = new RegExp(searchField, "i");
+        var radioFilter = $('input[name="foodType"]:checked').val();
+        $.getJSON("JSON/products.json", function (data) {
+            var output = "<ul class='searchResults'>";
+
             $.each(data, function (key, val) {
                 if (val.name.search(regExp) !== -1 ||
                     (val.about.search(regExp) !== -1) &&
-                    radioFilter ==) {
+                    radioFilter == val.type) {
                     output += "<li>"
                         + "<p>" + val.name + "</p>"
                         + "<p>" + val.about + "</p>"
@@ -18,6 +41,6 @@
             });
             output += "</ul>";
             $("#Information").html(output);
-        });
-    });
+        })
+    })
 })
