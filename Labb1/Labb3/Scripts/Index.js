409fc49b66fe0a6237ff9da71f7ee9dae43c97db
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-08 09:03:35 +0100

    Filtering in labb  finished

--- Labb1/Labb3/Scripts/Index.js
+++ Labb1/Labb3/Scripts/Index.js
@@ -21,26 +21,25 @@
             $("#Information").html(output);
         });
     });
-    $("#radio").change(function () {
-        var searchField = $(this).val();
-        var regExp = new RegExp(searchField, "i");
-        var radioFilter = $('input[name="foodType"]:checked').val();
+    $(".checkBox").on('click',function () {
+        var searchInput = $("#search").val();
+        var regExp = new RegExp(searchInput, "i");
         $.getJSON("JSON/products.json", function (data) {
             var output = "<ul class='searchResults'>";
-
             $.each(data, function (key, val) {
-                if (val.name.search(regExp) !== -1 ||
-                    (val.about.search(regExp) !== -1) &&
-                    radioFilter == val.type) {
-                    output += "<li>"
-                        + "<p>" + val.name + "</p>"
-                        + "<p>" + val.about + "</p>"
-                        + "<img src='" + val.image + "' alt='" + val.name + "'/>"
-                        + "</li>";
+                if ((val.name.search(regExp) !== -1) ||
+                    (val.about.search(regExp) !== -1)) {
+                    if ($("#" + val.type).is(":checked")) {
+                        output += "<li>"
+                            + "<p>" + val.name + "</p>"
+                            + "<p>" + val.about + "</p>"
+                            + "<img src='" + val.image + "' alt='" + val.name + "'/>"
+                            + "</li>";
+                    }
                 }
             });
             output += "</ul>";
             $("#Information").html(output);
-        })
-    })
+        });
+    });
 })
