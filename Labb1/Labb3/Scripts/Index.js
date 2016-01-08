7e98ac341a2ce5d2a86b03e20edc7157ed01bd65
Melos89 <Nicolausfugler_89@hotmail.com>
2016-01-08 11:05:10 +0100

    All done

--- Labb1/Labb3/Scripts/Index.js
+++ Labb1/Labb3/Scripts/Index.js
@@ -1,5 +1,18 @@
 ﻿$(document).ready(function () {
 
+    $.getJSON("JSON/products.json", function (data) {
+        var output = "<ul class='searchResults'>";
+
+        $.each(data, function (key, val) {
+            output += "<li>"
+                + "<p>" + val.name + "</p>"
+                + "<p>" + val.about + "</p>"
+                + "<img src='" + val.image + "' alt='" + val.name + "'/>"
+                + "</li>";
+        });
+        output += "</ul>";
+        $("#Information").html(output);
+    });
     $("#search").keyup(function () {
         var searchField = $(this).val();
         var regExp = new RegExp(searchField, "i");
@@ -21,15 +34,25 @@
             $("#Information").html(output);
         });
     });
-    $(".checkBox").on('click',function () {
+    $(".checkBox").on('click', function () {
+
         var searchInput = $("#search").val();
         var regExp = new RegExp(searchInput, "i");
+
         $.getJSON("JSON/products.json", function (data) {
             var output = "<ul class='searchResults'>";
+
             $.each(data, function (key, val) {
                 if ((val.name.search(regExp) !== -1) ||
                     (val.about.search(regExp) !== -1)) {
-                    if ($("#" + val.type).is(":checked")) {
+                    if ($("#everything").is(":checked")) {
+                        output += "<li>"
+                            + "<p>" + val.name + "</p>"
+                            + "<p>" + val.about + "</p>"
+                            + "<img src='" + val.image + "' alt='" + val.name + "'/>"
+                            + "</li>";
+                    }
+                    else if($("#" + val.type).is(":checked")) {
                         output += "<li>"
                             + "<p>" + val.name + "</p>"
                             + "<p>" + val.about + "</p>"
